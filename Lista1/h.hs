9a297f750eba60cb8922a75b8a507d598850c642
yasmws <yasminmwsoares@gmail.com>
2023-06-21 11:39:50 -0300

    h - done

--- Lista1/h.hs
+++ Lista1/h.hs
@@ -6,15 +6,12 @@ main = do
     let result = mul2 a b
     print result
 
-
-
 mul2 :: [Int] -> [Int] -> [Int]
-mul2 [] [] = smaller
-mul2 (a:as) (b:bs) = [loop a b] ++ : mul2 as bs   
+mul2 [] [] = []
+mul2 [] (x:xs) = (0 : (mul2 [] xs))
+mul2 (x:xs) [] = (0 : (mul2 xs []))
+mul2 (a:as) (b:bs) = (loop a b) : (mul2 as bs)   
 
-
-smaller :: [Int] -> [Int]
-smaller (a:as) (b:bs) = length 
 
 loop :: Int -> Int -> Int
 loop a b = a * b 

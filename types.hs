0be34b540db75115a819aace839f35d837c5ecb2
Yasmws <yasminmwsoares@gmail.com>
2023-07-03 08:38:34 -0300

    -

--- types.hs
+++ types.hs
@@ -2,3 +2,15 @@ clima :: Estacao -> Temp
 clima Inverno = Frio 
 clima _ = Quente 
 
+data Estacao = Inverno | Verao | Outono | Primavera
+            deriving (Show, Eq)
+
+data Temp = Frio | Quente
+            deriving (Show, Eq)
+
+
+clima2 :: Estacao -> Temp
+clima2 x | x == Inverno = Frio
+        | otherwise = Quente
+
+    

4edf3534eb5874b1f5ec9e381e334b0ef0e41f7f
yasmws <yasminmwsoares@gmail.com>
2023-07-03 08:53:03 -0300

    rename types.hs ->aula_8.hs

--- aula_8.hs
+++ aula_8.hs
@@ -2,12 +2,25 @@ clima :: Estacao -> Temp
 clima Inverno = Frio 
 clima _ = Quente 
 
+type Nome = String
+type Idade = Int
+
 data Estacao = Inverno | Verao | Outono | Primavera
             deriving (Show, Eq)
 
 data Temp = Frio | Quente
             deriving (Show, Eq)
 
+data Pessoas = Pessoa Nome Idade 
+            deriving (Show, Eq)
+
+Pessoa "José" 22
+Pessoa "Maria" 23
+
+showPessoa :: Pessoas -> String
+showPessoa (Pessoa n a) = n ++ "--" ++ show a 
+
+Pessoa :: Nome -> Idade -> Pessoas
 
 clima2 :: Estacao -> Temp
 clima2 x | x == Inverno = Frio
